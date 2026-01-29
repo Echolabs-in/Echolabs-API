9228cd4230c15873c60e71f0399c7dbf3416a51e
adityagithubraj <adityaraj9843@gmail.com>
2026-01-29 16:46:31 +0530

    fixed

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -8,14 +8,25 @@ try {
   // Ignore - environment variables are available in Netlify
 }
 
-// Import the Express app using absolute path resolution
+// Set NODE_PATH to help with module resolution in Netlify Functions
+// Netlify bundles everything to /var/task, so we need to help Node find modules
+process.env.NODE_PATH = path.join(process.cwd(), "node_modules") + 
+  (process.env.NODE_PATH ? path.delimiter + process.env.NODE_PATH : "");
+
+// Try to require the app - Netlify bundles src/ to the function root
 let app;
 try {
-  const appPath = path.resolve(__dirname, "../../src/index.js");
-  app = require(appPath);
-} catch (err) {
-  console.error("Failed to load Express app:", err);
-  throw err;
+  // First try: relative path from function location (after bundling)
+  app = require("../../src/index");
+} catch (err1) {
+  try {
+    // Second try: from project root (process.cwd() should be /var/task)
+    const appPath = path.join(process.cwd(), "src", "index");
+    app = require(appPath);
+  } catch (err2) {
+    // Third try: direct require assuming src is at root level after bundling
+    app = require("./src/index");
+  }
 }
 
 // Export the Express app wrapped in serverless-http for Netlify Functions
